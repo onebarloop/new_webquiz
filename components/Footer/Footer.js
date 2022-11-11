3d820fa0c86d7172cdaf10235106e2f1366cb413
Alexander Bayer <alexanderbayer@posteo.de>
2022-11-11 13:44:01 +0100

    add footer

---
+++ components/Footer/Footer.js
@@ -0,0 +1,11 @@
+export function Footer() {
+  const header = document.createElement("header");
+  header.innerHTML = `
+      <ul>
+        <li>Home</li>
+        <li>Form</li>
+      
+      </ul>
+      `;
+  return header;
+}
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { Header } from "./components/Header/Header.js";
 import { Card } from "./components/Cards/Card.js";
+import { Footer } from "./components/Footer/Footer.js";
 
 const root = document.getElementById("root");
 
 root.append(Header());
 root.append(Card());
+root.append(Footer());
