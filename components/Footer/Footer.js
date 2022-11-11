790cc351dbae6af790b53e5333b9446d4f172e0b
Alexander Bayer <alexanderbayer@posteo.de>
2022-11-11 14:21:26 +0100

    Add Event Listeners to footer items

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,11 +1,19 @@
 export function Footer() {
-  const header = document.createElement("header");
-  header.innerHTML = `
-      <ul>
-        <li>Home</li>
-        <li>Form</li>
-      
-      </ul>
-      `;
-  return header;
+  const footer = document.createElement("footer");
+  const navbar = document.createElement("ul");
+  footer.append(navbar);
+  const home = document.createElement("li");
+  home.textContent = "HOME";
+  home.addEventListener("click", () => {
+    console.log("home clicked");
+  });
+  navbar.append(home);
+  const form = document.createElement("li");
+  form.addEventListener("click", () => {
+    console.log("form clicked");
+  });
+  form.textContent = "New Questions";
+  navbar.append(form);
+
+  return footer;
 }
